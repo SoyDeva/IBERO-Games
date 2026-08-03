42fe4790120bc79fa36ed84f1a4e4035bbd2b58a
Danilo Olarte González <aprendevata@gmail.com>
2026-08-03 13:55:19 -0500

    refactor: centralize storage keys

---
+++ js/config/storage-keys.js
@@ -0,0 +1,10 @@
+export const STORAGE_KEYS = Object.freeze({
+  savedGame: 'mision-nebula-save-v1',
+  settings: 'mision-nebula-settings-v1',
+  economy: 'nebula-economy-v1',
+  pilotProfile: 'nebula-pilot-profile',
+  achievements: 'nebula-achievements',
+  tutorialComplete: 'nebula-tutorial-complete',
+  flightBest: 'nebula-flight-best',
+  rankingPrefix: 'nebula-ranking-'
+});
